383b7c4369dca98e518b039236523d123bfc5703
vikapetrovskaya <vika.petrovskaya@ukr.net>
2024-12-03 20:15:25 +0200

    second commit

---
+++ my_first_python_class.py
@@ -0,0 +1,32 @@
+
+data = 'Hello World'
+
+first_list = [10, 'some string', 55, "rather string"]
+
+integer = 10
+
+data_type = type(first_list)
+
+print(len(first_list), data_type, data, integer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
